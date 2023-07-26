f616f62b074485b481eaae036a1217b4f9f84b26
via-chi28 <139003497+via-chi28@users.noreply.github.com>
2023-07-26 18:56:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 //Paste your firebaseConfig here
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDKDeJTEwYb3NqCXeJu3ogXMD7RW9ONwm0",
+  authDomain: "e-ride-stg4.firebaseapp.com",
+  projectId: "e-ride-stg4",
+  storageBucket: "e-ride-stg4.appspot.com",
+  messagingSenderId: "595416782120",
+  appId: "1:595416782120:web:4fdb8520cedc26f1abac1d"
+};
 //
 firebase.initializeApp(firebaseConfig);
 
